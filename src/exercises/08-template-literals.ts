ec854a1d5f53485a266928d226a0eeeed4be351a
sijabulile-ncube <s.mabhena@gmail.com>
2026-07-01 17:52:34 +0200

    add exercise 8.template-literals.ts

--- src/exercises/08-template-literals.ts
+++ src/exercises/08-template-literals.ts
@@ -1,1 +1,18 @@
-.
+export type Entity = "user" | "order";
+export type Act = "created" | "deleted";
+
+export type EventName = `${Entity}:${Act}`;
+
+export const ev: EventName = "order:created";
+// @ts-expect-error not a valid entity:action pair
+export const badEv: EventName = "user:updated";
+
+export type RouteWithId<T extends string> = `/${T}/:id`;
+
+export const r: RouteWithId<"users"> = "/users/:id";
+
+export type Prefixed<T, P extends string> = {
+  [K in keyof T as `${P}_${string & K}`]: T[K];
+};
+
+export const cfg: Prefixed<{ a: number; b: string }, "cfg"> = { cfg_a: 1, cfg_b: "x" };
